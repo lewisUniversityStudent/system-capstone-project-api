208da405cf2e5729cc950f0a0992f6710e0d2b31
root <syedaraza01@lewisu.edu>
2021-05-09 01:39:11 +0000

    Update RegistrationController

---
+++ src/main/java/com/raza/marketapp/controller/RegistrationController.java
@@ -0,0 +1,37 @@
+package com.raza.marketapp.controller;
+
+import java.util.Date;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import static java.lang.Math.*;
+
+import com.google.gson.*;
+import com.raza.marketapp.model.*;
+
+@RestController
+@RequestMapping("api")
+public class LoginController {
+
+        public static bool existsUser(String username)
+	{
+		return (String)jdbcTemplate.queryForObject("select username from users where username='" + username + "';") != null;
+	}
+
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
+	@ResponseBody
+	public String login(@RequestParam(required = true) String username, @RequestParam(required = true) String password, @RequestParam(required = true) String name, @RequestParam(required = true) String surname) {
+          if (existsUser(username)) return "Username already taken!";
+          jdbcTemplate.queryForObject("insert into users(username, password, name, surname) values('" + + username + "','" + password + "','" + name + "','" + surname + "';");
+	  return "Success";
+	}
+
+        @Bean
+ 	public JdbcTemplate jdbcTemplate(DataSource dataSource)
+	{
+   	 	return new JdbcTemplate(dataSource);
+	}
+}
